422fc7f26500c4c1e27b9e2e4db806be5ac5d365
Ntwali Brian <ntwalibriand@gmail.com>
2024-11-19 16:52:51 +0200

    footerr

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -5,7 +5,7 @@ interface Props {
 function Label({title}:Props) {
   return (
     <span className="text-md text-darkBlue bg-lightBlue rounded-3xl py-1 px-2 font-semibold cursor-default w-fit">{title}</span>
-  )
+  );
 }
 
 export default Label
