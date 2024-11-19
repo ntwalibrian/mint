2c2eeac6c345cc64c9d59639c00b25c1af02116f
Davy <103334758+Descode1@users.noreply.github.com>
2024-11-19 16:10:04 +0200

    merger

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,14 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import FAQs from "../components/FAQs";
 import Newsletter from "../components/Newsletter";
+import FeatureSection from "../components/FeatureSection";
 const Home = () => {
   return (
     <>
     <Navbar/>
     <Hero/>
-   
+   <FeatureSection/>
+   <FAQs/>
    
    </>
   )}
